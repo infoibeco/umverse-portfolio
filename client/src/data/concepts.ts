9d223c89f27c0de8da5cb72aa15021d49f1a5a37
infoibeco <info.ibeco@gmail.com>
2026-08-13 19:33:51 +0900

    update

--- client/src/data/concepts.ts
+++ client/src/data/concepts.ts
@@ -37,44 +37,7 @@ teaserUrl: "https://youtu.be/2T9VjpR2qq4",
     // teaserUrl: "https://youtu.be/xxxxxxxx", // 티저 완성되면 여기에 링크 추가
   },
 
-  {
-    id: "the-gift",
-    title: "The Gift",
-    logline: "A short-form IP foundation series within the UMVERSE system.",
-    imageUrl: "/the-gift.png",
-  },
-  {
-    id: "pants",
-    title: "What happened to my pants?",
-    logline: "A short-form IP foundation series within the UMVERSE system.",
-    imageUrl: "/pants.png",
-  },
- {
-    id: "cicada-sleep",
-    title: "Cicada Sleep",
-    logline: "A short-form IP foundation series within the UMVERSE system.",
-    imageUrl: "/cicada-sleep.png",
-    // teaserUrl: "https://youtu.be/xxxxxxxx", // 티저 완성되면 여기에 링크 추가
-  },
 
-  {
-    id: "dispenser",
-    title: "The dispenser",
-    logline: "A short-form IP foundation series within the UMVERSE system.",
-    imageUrl: "/dispenser.png",
-  },
-  {
-    id: "amniotic",
-    title: "Amniotic Rain",
-    logline: "A short-form IP foundation series within the UMVERSE system.",
-    imageUrl: "/amniotic.png",
-  },
-  {
-    id: "emotion-gum",
-    title: "The Gum",
-    logline: "A short-form IP foundation series within the UMVERSE system.",
-    imageUrl: "/emotion-gum.png",
-  },
  {
     id: "reset",
     title: "Reset",
@@ -96,6 +59,54 @@ teaserUrl: "https://youtu.be/2T9VjpR2qq4",
     imageUrl: "/gamguma.png",
     // teaserUrl: "https://youtu.be/xxxxxxxx", // 티저 완성되면 여기에 링크 추가
   },
+
+ 
+
+  {
+    id: "dispenser",
+    title: "The dispenser",
+    logline: "A short-form IP foundation series within the UMVERSE system.",
+    imageUrl: "/dispenser.png",
+  },
+  {
+    id: "amniotic",
+    title: "Amniotic Rain",
+    logline: "A short-form IP foundation series within the UMVERSE system.",
+    imageUrl: "/amniotic.png",
+  },
+  {
+    id: "emotion-gum",
+    title: "The Gum",
+    logline: "A short-form IP foundation series within the UMVERSE system.",
+    imageUrl: "/emotion-gum.png",
+  },
+
+
+
+ {
+    id: "the-gift",
+    title: "The Gift",
+    logline: "A short-form IP foundation series within the UMVERSE system.",
+    imageUrl: "/the-gift.png",
+  },
+  {
+    id: "pants",
+    title: "What happened to my pants?",
+    logline: "A short-form IP foundation series within the UMVERSE system.",
+    imageUrl: "/pants.png",
+  },
+ {
+    id: "cicada-sleep",
+    title: "Cicada Sleep",
+    logline: "A short-form IP foundation series within the UMVERSE system.",
+    imageUrl: "/cicada-sleep.png",
+    // teaserUrl: "https://youtu.be/xxxxxxxx", // 티저 완성되면 여기에 링크 추가
+  },
+
+
+
+
+
  {
     id: "village",
     title: "Village Sunsong",
