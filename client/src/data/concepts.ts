bd55ffe0e2c2698c9edf4e3b7964b7217a836854
infoibeco <info.ibeco@gmail.com>
2026-08-15 18:46:16 +0900

    update

--- client/src/data/concepts.ts
+++ client/src/data/concepts.ts
@@ -108,19 +108,18 @@ teaserUrl: "https://youtu.be/2T9VjpR2qq4",
  
 
 
-  {
-    id: "romeo-juliet",
-    title: "Romeo and Juliet Alpha",
-    logline: "A short-form IP foundation series within the UMVERSE system.",
-    imageUrl: "/romeo-juliet.png",
-    // teaserUrl: "https://youtu.be/xxxxxxxx", // 티저 완성되면 여기에 링크 추가
-  },
+
 {
-    id: "hero",
-    title: "Hedgehog Man",
+    id: "the-gift",
+    title: "The Gift",
     logline: "A short-form IP foundation series within the UMVERSE system.",
-    imageUrl: "/hero.png",
-    // teaserUrl: "https://youtu.be/xxxxxxxx", // 티저 완성되면 여기에 링크 추가
+    imageUrl: "/the-gift.png",
+  },
+ {
+    id: "zombie",
+    title: "Zombie Bugs",
+    logline: "A short-form IP foundation series within the UMVERSE system.",
+    imageUrl: "/zombie.png",
   },
 {
     id: "jellyfish",
@@ -142,12 +141,12 @@ teaserUrl: "https://youtu.be/2T9VjpR2qq4",
     imageUrl: "/village.png",
     // teaserUrl: "https://youtu.be/xxxxxxxx", // 티저 완성되면 여기에 링크 추가
   }, 
- 
  {
-    id: "zombie",
-    title: "Zombie Bugs",
+    id: "hero",
+    title: "Hedgehog Man",
     logline: "A short-form IP foundation series within the UMVERSE system.",
-    imageUrl: "/zombie.png",
+    imageUrl: "/hero.png",
+    // teaserUrl: "https://youtu.be/xxxxxxxx", // 티저 완성되면 여기에 링크 추가
   },
 {
     id: "cicada-sleep",
@@ -160,11 +159,12 @@ teaserUrl: "https://youtu.be/2T9VjpR2qq4",
 
 
 
-{
-    id: "the-gift",
-    title: "The Gift",
+  {
+    id: "romeo-juliet",
+    title: "Romeo and Juliet Alpha",
     logline: "A short-form IP foundation series within the UMVERSE system.",
-    imageUrl: "/the-gift.png",
+    imageUrl: "/romeo-juliet.png",
+    // teaserUrl: "https://youtu.be/xxxxxxxx", // 티저 완성되면 여기에 링크 추가
   },
 {
     id: "hair",
